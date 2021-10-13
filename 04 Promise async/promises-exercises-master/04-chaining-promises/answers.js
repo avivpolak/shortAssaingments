f13d7f0aa562debeec51d7323b71c0d34e21f2f2
AVIV POLAK <avivp0437@gmail.com>
2021-10-13 19:16:07 +0300

    04 exercise 1

--- 04 Promise async/promises-exercises-master/04-chaining-promises/answers.js
+++ 04 Promise async/promises-exercises-master/04-chaining-promises/answers.js
@@ -1,43 +1,48 @@
 /**
- * 
+ *
  * EXERCISE 1
- * 
- * @param {Promise} promise 
- * @param {function} asyncTransformer 
+ *
+ * @param {Promise} promise
+ * @param {function} asyncTransformer
  */
-function flatMapPromise(promise, asyncTransformer){
-  return new Promise((resolve, reject) => {
-    promise
-      .then(/* IMPLEMENT ME! */);
-  });
+function flatMapPromise(promise, asyncTransformer) {
+    return new Promise((resolve, reject) => {
+        promise
+            .then((response) => {
+                resolve(asyncTransformer(response));
+            })
+            .catch((error) => {
+                reject(error);
+            });
+    });
 }
 
 /**
- * 
+ *
  * EXERCISE 2
- * 
- * @param {Promise} firstPromise 
- * @param {function} slowAsyncProcess 
+ *
+ * @param {Promise} firstPromise
+ * @param {function} slowAsyncProcess
  */
-function chainTwoAsyncProcesses(firstPromise, slowAsyncProcess){
-  return firstPromise.then(/* IMPLEMENT ME! */);
+function chainTwoAsyncProcesses(firstPromise, slowAsyncProcess) {
+    return firstPromise.then(/* IMPLEMENT ME! */);
 }
 
 /**
- * 
+ *
  * EXERCISE 3
- * 
- * @param {function} getUserById 
- * @param {function} getOrganizationById 
+ *
+ * @param {function} getUserById
+ * @param {function} getOrganizationById
  */
-function makeGetUserByIdWithOrganization(getUserById, getOrganizationById){
-  return function getUserByIdWithOrganization(userId){
-    /* IMPLEMENT ME! */
-  };
+function makeGetUserByIdWithOrganization(getUserById, getOrganizationById) {
+    return function getUserByIdWithOrganization(userId) {
+        /* IMPLEMENT ME! */
+    };
 }
 
 module.exports = {
-  flatMapPromise,
-  chainTwoAsyncProcesses,
-  makeGetUserByIdWithOrganization,
+    flatMapPromise,
+    chainTwoAsyncProcesses,
+    makeGetUserByIdWithOrganization,
 };
