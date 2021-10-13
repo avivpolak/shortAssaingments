961b2e850e261ba8e4944bdf97cfedae095394ae
AVIV POLAK <avivp0437@gmail.com>
2021-10-13 16:24:51 +0300

    switch to backslash

--- 04 Promise async/promises-exercises-master/01-making-promises/answers.js
+++ 04 Promise async/promises-exercises-master/01-making-promises/answers.js
@@ -1,54 +1,54 @@
-
 /**
- * 
+ *
  * EXERCISE 1
- * 
+ *
  * @returns {Promise<3>}
  */
-function makePromiseResolveWith3(){
-  /* IMPLEMENT ME! */
+function makePromiseResolveWith3() {
+    return new Promise().resolve(3);
+    /* IMPLEMENT ME! */
 }
 
 /**
- * 
+ *
  * EXERCISE 2
- * 
+ *
  * @returns {Promise<,"Boo!">}
  */
-function makePromiseRejectWithBoo(){
-  /* IMPLEMENT ME! */
+function makePromiseRejectWithBoo() {
+    /* IMPLEMENT ME! */
 }
 
 /**
- * 
+ *
  * EXERCISE 3
- * 
+ *
  * @param {boolean} itShouldResolve - Whether the promise should resolve or reject
  * @returns {Promise<undefined, undefined>}
  */
 
-function makePromiseWithConstructor(itShouldResolve){
-  return new Promise((resolve, reject) => {
-    /* If itShouldResolve is true, call resolve */
-    /* If itShouldResolve is false, call reject */
-  });
+function makePromiseWithConstructor(itShouldResolve) {
+    return new Promise((resolve, reject) => {
+        /* If itShouldResolve is true, call resolve */
+        /* If itShouldResolve is false, call reject */
+    });
 }
 
 /**
- * 
+ *
  * EXERCISE 4
  *
- * @param {any} value 
- * @param {number} delayInMs 
+ * @param {any} value
+ * @param {number} delayInMs
  * @return {Promise<any>} - A promise that will resolve with the value after delayInMs milliseconds
  */
-function makeDelayPromise(value, delayInMs){
-  /* Return a promise that resolves with the value after delayInMs */
+function makeDelayPromise(value, delayInMs) {
+    /* Return a promise that resolves with the value after delayInMs */
 }
 
 module.exports = {
-  makePromiseResolveWith3,
-  makePromiseRejectWithBoo,
-  makePromiseWithConstructor,
-  makeDelayPromise,
+    makePromiseResolveWith3,
+    makePromiseRejectWithBoo,
+    makePromiseWithConstructor,
+    makeDelayPromise,
 };
