5d106b9b9f82149000110d18cf0dfcca63b7e0a0
AVIV POLAK <avivp0437@gmail.com>
2021-10-17 21:39:47 +0300

    divide into files

---
+++ 06.modules/calc/app/helpers/eventHandlers.js
@@ -0,0 +1,35 @@
+import {
+    handleAction,
+    handleEqual,
+    handleDel,
+    handleNum,
+} from "./eventHelpers";
+import { state } from "./state";
+
+export function handleClick(e) {
+    const target = e.target.value;
+    if (!state.action) {
+        state.flag = "n1";
+    }
+    if ("1234567890.".includes(target) && e.target.id !== "result") {
+        handleNum(target);
+    }
+    if ("+".includes(target)) {
+        handleAction("add");
+    }
+    if ("-".includes(target)) {
+        handleAction("sub");
+    }
+    if ("/".includes(target)) {
+        handleAction("div");
+    }
+    if ("X".includes(target)) {
+        handleAction("mult");
+    }
+    if ("=".includes(target)) {
+        handleEqual();
+    }
+    if ("Del".includes(target)) {
+        handleDel();
+    }
+}
